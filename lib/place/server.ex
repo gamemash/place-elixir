927953d0e1df2e262c34a4f9c832763c5b1812ef
Oliver Ponder <oliver.ponder@gmail.com>
2017-04-15 17:17:38 +0200

    some helper methods

--- lib/place/server.ex
+++ lib/place/server.ex
@@ -14,6 +14,26 @@ defmodule Place.Server do
     {:ok, []}
   end
 
+  def set_pixel(pid, pixel = %Pixel{}) do
+    GenServer.cast(pid, pixel)
+  end
+
+  def get_board(pid) do
+    GenServer.call(pid, :board)
+  end
+
+  def get_history(pid) do
+    GenServer.call(pid, :state)
+  end
+
+  def clear_board(pid) do
+    GenServer.cast(pid, :clear_board)
+  end
+
+  def clear_history(pid) do
+    GenServer.cast(pid, :clear_history)
+  end
+
   def handle_call(:board, _from, log) do
     board = Enum.reduce(log, empty_board(), fn pixel, board ->
       List.replace_at(board, pixel.x + pixel.y * @width, pixel.color)
@@ -30,7 +50,16 @@ defmodule Place.Server do
     {:noreply, state}
   end
 
+  def handle_cast(:clear_history, _) do
+    {:noreply, []}
+  end
+
+  def handle_cast(:clear_board, state) do
+    clearPixels = for x <- 0..(@width - 1), y <- 0..(@height - 1), do: %Pixel{x: x, y: y, color: 0}
+    {:noreply, clearPixels ++ state}
+  end
+
   defp empty_board() do
-    for x <- 0..(@width - 1), y <- 0..(@height - 1), do: 0
+    for _ <- 0..(@width - 1), _ <- 0..(@height - 1), do: 0
   end
 end
--- test/place_test.exs
+++ test/place_test.exs
@@ -4,8 +4,28 @@ defmodule PlaceTest do
 
   test "board" do
     {:ok, pid} = Place.Server.start_link
-    GenServer.cast(pid, %Place.Pixel{color: 10})
+
+    Place.Server.set_pixel(pid, %Place.Pixel{color: 10})
     {:ok, board} = GenServer.call(pid, :board)
     assert Enum.at(board, 0) == 10
+  end
+
+  test "clear history" do
+    {:ok, pid} = Place.Server.start_link
+
+    Place.Server.set_pixel(pid, %Place.Pixel{color: 10})
+    Place.Server.clear_history(pid)
+
+    {:ok, board} = Place.Server.get_board(pid)
+    assert Enum.at(board, 0) == 0
+  end
+
+  test "clear board" do
+    {:ok, pid} = Place.Server.start_link
+
+    Place.Server.set_pixel(pid, %Place.Pixel{color: 10})
+    Place.Server.clear_board(pid)
+    {:ok, history} = Place.Server.get_history(pid)
+    assert Enum.at(history, 100).color == 10
   end
 end
