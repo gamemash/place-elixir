9a5ff37dcf94a273cff47bdcd887c7e6dd5c968b
Ronald Hecker <ronald.m.hecker@gmail.com>
2017-04-15 16:42:48 +0200

    Create default state

--- lib/place/server.ex
+++ lib/place/server.ex
@@ -3,12 +3,16 @@ defmodule Place.Server do
 
   alias Place.Pixel
 
+  @width  10
+  @height 10
+
   def start_link do
     GenServer.start_link(__MODULE__, :ok, [])
   end
 
   def init(:ok) do
-    {:ok, []}
+    state = for x <- 0..@width, y <- 0..@height, do: %Pixel{x: x, y: y, color: 0}
+    {:ok, state}
   end
 
   def handle_call(:state, _from, state) do
